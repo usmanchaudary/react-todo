ae9d33a23b1615f65ee98e7b7426cb78d85a61f4
Shahmir Khan Jadoon <shahmirkj@gmail.com>
2022-09-01 15:06:49 +0500

    Fixed routing issue

--- src/Routing/Router.js
+++ src/Routing/Router.js
@@ -1,17 +1,14 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import App from "../App";
-import Hello from "../Components/Hello";
-import HelloCopy from "../Components/Hello copy";
-import SearchBar from "../Components/SearchBar";
-const Router = () => {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/hello" element={<Hello />} />
-                <Route exact path="/" element={<HelloCopy />} />
+import { Routes, Route } from 'react-router-dom';
 
-            </Routes>
-        </BrowserRouter>
-    );
-}
+import Hello from '../Components/Hello';
+import HelloCopy from '../Components/Hello copy';
+
+const Router = () => {
+	return (
+		<Routes>
+			<Route path='/hello' element={<Hello />} />
+			<Route exact path='/' element={<HelloCopy />} />
+		</Routes>
+	);
+};
 export default Router;
